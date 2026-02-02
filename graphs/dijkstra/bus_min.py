6eb830330e50ac0a991a481666b2a49ff6735824
GaegulX <mjkim11084@gmail.com>
2026-02-02 13:54:56 +0900

    Update Many Many Things And Write Solution for Algospot, Almost Shtortest Path, Min dist with bus, Zelda

--- graphs/dijkstra/bus_min.py
+++ graphs/dijkstra/bus_min.py
@@ -1,4 +1,6 @@
 import heapq
+import sys
+input=sys.stdin.readline
 class Graph:
     def __init__(self, n, zb):
         """
@@ -62,29 +64,17 @@ class Graph:
             cur = self.prev[cur]
         path.reverse()
         return path
-class GridGraph:
-    def __init__(self, grid):
-        self.grid = grid
-        self.h = len(grid)
-        self.w = len(grid[0])
-        self.dx = [0, 0, 1, -1]
-        self.dy = [1, -1, 0, 0]
-    def LazyDijkstra(self, atx, aty):
-        INF = 10**18
-        dist = [[INF]*self.w for _ in range(self.h)]
-        hq = []
-        dist[atx][aty] = self.grid[atx][aty]
-        heapq.heappush(hq, (dist[atx][aty], atx, aty))
-        while hq:
-            cd, x, y = heapq.heappop(hq)
-            if cd > dist[x][y]:
-                continue
-            for i in range(4):
-                nx = x + self.dx[i]
-                ny = y + self.dy[i]
-                if 0 <= nx < self.h and 0 <= ny < self.w:
-                    nd = cd + self.grid[nx][ny]
-                    if nd < dist[nx][ny]:
-                        dist[nx][ny] = nd
-                        heapq.heappush(hq, (nd, nx, ny))
-        return dist
+n = int(input())
+m = int(input())
+graph = Graph(n, False)
+for _ in range(m):
+    u, v, w = map(int, input().split())
+    graph.add_edge(u, v, w)
+a, b = map(int, input().split())
+dist, _ = graph.LazyMinRoute(a)
+a -= 1
+b -= 1
+path = graph.restore_path(a, b)
+print(dist[b])
+print(len(path))
+print(*[x+1 for x in path])
